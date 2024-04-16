ae2e6a1a0cae7e122bc235a331384400b750e461
chinna421 <108382623+chinna421@users.noreply.github.com>
2024-04-16 17:52:36 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "dev-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "demo-vpc"
+    Name = "demo-vpc-dev"
     Purpose = "Jenkins Demo New Added Tag"
   }
 }
